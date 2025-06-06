6f7266e9acdec561a86724568e73c2718c4f5ca1
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-06-06 16:12:46 +1000

    first commit

---
+++ src/main/java/com/feelsuegood/letsue_spring/LetsueSpringApplication.java
@@ -0,0 +1,13 @@
+package com.feelsuegood.letsue_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsueSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsueSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feelsuegood/letsue_spring/LetsueSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feelsuegood.letsue_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsueSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
